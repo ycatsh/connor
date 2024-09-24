13fe3690c44bc9d787cecd3297b6b81924a16451
ycatsh <github.418@mail.goyalyash.com>
2024-09-24 14:19:15 +0200

    fix: default run mode is cli

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from cli.main import main as cli_main
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='Connor', description="Connor DEV: Run the application in GUI or CLI mode.")
     parser.add_argument('--gui', action='store_true', help='Run the application in GUI mode.')
-    args = parser.parse_args()
+    args, unknown = parser.parse_known_args()
 
     if args.gui:
         gui_main()
