b78c8900be747d3275b0f56842311ad6af678538
ycatsh <github.418@mail.goyalyash.com>
2024-09-24 08:30:27 +0200

    feat: configured run flags

--- run.py
+++ run.py
@@ -5,13 +5,10 @@ from cli.main import main as cli_main
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='Connor', description="Connor DEV: Run the application in GUI or CLI mode.")
-    parser.add_argument('--cli', action='store_true', help='Run the application in CLI mode.')
     parser.add_argument('--gui', action='store_true', help='Run the application in GUI mode.')
     args = parser.parse_args()
 
-    if args.cli:
-        cli_main()
-    elif args.gui:
+    if args.gui:
         gui_main()
     else:
-        print("No mode specified. Use --cli for CLI mode or --gui for GUI mode.")
+        cli_main()
